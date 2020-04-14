599b23d1cf18193682dce02ade87f4f7d8f4b5b0
camilavillalobos1 <civillalobos@miuandes.cl>
2020-04-14 15:47:40 -0400

    First commit

---
+++ Lab4_poo_camila/Lab4_poo_camila/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab4_poo_camila
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
